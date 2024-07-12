d98197e410e1abd94f5398d91e29e61b01de47ff
AleksandarHaralanov <aleksandar.haralanov01@gmail.com>
2024-07-12 19:29:57 +0300

    Info command

---
+++ src/main/java/com/haralanov/milksquids/MilkSquidsCommand.java
@@ -0,0 +1,41 @@
+package com.haralanov.milksquids;
+
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.Command;
+import org.bukkit.entity.Player;
+import org.bukkit.ChatColor;
+
+import static org.bukkit.Bukkit.getLogger;
+
+public class MilkSquidsCommand implements CommandExecutor {
+
+    private final String NAME;
+    private final String VERSION;
+    private final String AUTHOR;
+    private final String SOURCE;
+
+    public MilkSquidsCommand(final String NAME, final String VERSION, final String AUTHOR, final String SOURCE) {
+        this.NAME = NAME;
+        this.VERSION = VERSION;
+        this.AUTHOR = AUTHOR;
+        this.SOURCE = SOURCE;
+    }
+
+    @Override
+    public boolean onCommand(final CommandSender commandSender, final Command command, final String s, final String[] strings) {
+        final Player player = (commandSender instanceof Player) ? (Player) commandSender : null;
+
+        if (player != null) {
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                    String.format("&e%s v%s &bby &e%s", NAME, VERSION, AUTHOR)));
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                    String.format("&bSource: &e%s", SOURCE)));
+        } else {
+            getLogger().info(String.format("%s v%s by %s", NAME, VERSION, AUTHOR));
+            getLogger().info(String.format("Source: %s", SOURCE));
+        }
+
+        return true;
+    }
+}
